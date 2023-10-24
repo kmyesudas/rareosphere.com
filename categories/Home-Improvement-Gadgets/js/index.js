d08d335343ab22f6aa82b5184e30d91a44316c91
YesudasKM <you@example.com>
2023-10-25 00:20:36 +0530

    update

--- categories/Home-Improvement-Gadgets/js/index.js
+++ categories/Home-Improvement-Gadgets/js/index.js
@@ -1,14 +1,14 @@
 // Function to open a new window for sharing on Facebook
 function shareOnFacebook() {
     var url = encodeURIComponent(window.location.href);
-    window.open('https://www.facebook.com/sharer/sharer.php?u=' + url, 'Share on Facebook', 'width=600, height=400');
+    window.open('https://www.facebook.com/sharer/sharer.php?u=' + url, 'Share on Facebook');
 }
 
 // Function to share on Twitter
 function shareOnTwitter() {
     var url = encodeURIComponent(window.location.href);
     var text = encodeURIComponent('Check out this awesome website!');
-    window.open('https://twitter.com/intent/tweet?url=' + url + '&text=' + text, 'Share on Twitter', 'width=600, height=400');
+    window.open('https://twitter.com/intent/tweet?url=' + url + '&text=' + text, 'Share on Twitter');
 }
 
 // Function to share on LinkedIn
@@ -16,19 +16,19 @@ function shareOnLinkedIn() {
     var url = encodeURIComponent(window.location.href);
     var title = encodeURIComponent('Your Website Title');
     var summary = encodeURIComponent('Description of your website');
-    window.open('https://www.linkedin.com/sharing/share-offsite/?url=' + url + '&title=' + title + '&summary=' + summary, 'Share on LinkedIn', 'width=600, height=400');
+    window.open('https://www.linkedin.com/sharing/share-offsite/?url=' + url + '&title=' + title + '&summary=' + summary, 'Share on LinkedIn');
 }
 
 // Function to share on WhatsApp
 function shareOnWhatsApp() {
     var url = encodeURIComponent(window.location.href);
-    window.open('https://api.whatsapp.com/send?text=' + url, 'Share on WhatsApp', 'width=600, height=400');
+    window.open('https://api.whatsapp.com/send?text=' + url, 'Share on WhatsApp');
 }
 
 // Function to share on Telegram
 function shareOnTelegram() {
     var url = encodeURIComponent(window.location.href);
-    window.open('https://t.me/share/url?url=' + url, 'Share on Telegram', 'width=600, height=400');
+    window.open('https://t.me/share/url?url=' + url, 'Share on Telegram');
 }
 
 // Function to share on Pinterest
