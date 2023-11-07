0d7fe585140c9c084930b67b27feaf568ec13be7
YesudasKM <you@example.com>
2023-11-07 23:57:56 +0530

    update

---
+++ js/random.js
@@ -0,0 +1,32 @@
+function getRandomIndices(array, count) {
+    const indices = [];
+    while (indices.length < count) {
+        const index = Math.floor(Math.random() * array.length);
+        if (!indices.includes(index)) {
+            indices.push(index);
+        }
+    }
+    return indices;
+}
+
+function displayRandomCards() {
+    const productData = JSON.parse(localStorage.getItem('productData'));
+    const productContainer = document.getElementById('product-container');
+    productContainer.innerHTML = '';
+
+    const randomIndices = getRandomIndices(productData, 4);
+    randomIndices.forEach(index => {
+        const card = document.createElement('div');
+        card.className = 'product-card-random';
+        card.innerHTML = productData[index];
+        productContainer.appendChild(card);
+    });
+}
+
+// Initial display
+displayRandomCards();
+
+// Refresh every 5 minutes
+setInterval(() => {
+    displayRandomCards();
+}, 2 * 60 * 1000);
