5064258db9b116b770d776bb9635666e9b6d916d
YesudasKM <you@example.com>
2023-11-08 09:20:38 +0530

    update

--- js/random.js
+++ js/random.js
@@ -1,24 +1,31 @@
 function getRandomIndices(array, count) {
     const indices = [];
     while (indices.length < count) {
-        const database = Math.floor(Math.random() * array.length);
-        if (!indices.includes(database)) {
-            indices.push(database);
+        const index = Math.floor(Math.random() * array.length);
+        if (!indices.includes(index)) {
+            indices.push(index);
         }
     }
     return indices;
 }
 
 function displayRandomCards() {
+    // Retrieve product data from local storage
     const productData = JSON.parse(localStorage.getItem('productData'));
+
+    if (!productData) {
+        console.log('No product data found in local storage.');
+        return;
+    }
+
     const productContainer = document.getElementById('product-container');
     productContainer.innerHTML = '';
 
     const randomIndices = getRandomIndices(productData, 4);
-    randomIndices.forEach(database => {
+    randomIndices.forEach(index => {
         const card = document.createElement('div');
-        card.className = 'product-card-random';
-        card.innerHTML = productData[database];
+        card.className = 'product-card';
+        card.innerHTML = productData[index];
         productContainer.appendChild(card);
     });
 }
@@ -29,4 +36,4 @@ displayRandomCards();
 // Refresh every 5 minutes
 setInterval(() => {
     displayRandomCards();
-}, 2 * 60 * 1000);
+}, 5 * 60 * 1000);
