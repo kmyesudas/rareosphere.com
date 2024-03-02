01bf804588371282f844843b7b1173aacf8036bc
YesudasKM <you@example.com>
2024-03-02 15:15:53 +0530

    update

--- js/custom.js
+++ js/custom.js
@@ -6,3 +6,22 @@ function getYear() {
 }
 
 getYear();
+
+// header show and hide
+let lastScrollTop = 0;
+const header = document.getElementById('header');
+
+window.addEventListener('scroll', () => {
+  let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+  if (scrollTop > lastScrollTop) {
+    // Scroll down
+    header.style.top = `-${header.offsetHeight}px`;
+  } else {
+    // Scroll up
+    header.style.top = '0';
+  }
+
+  lastScrollTop = scrollTop;
+});
+
