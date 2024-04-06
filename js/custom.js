75e0c3ab73210ceaa091f42ab7847424a9f234e6
YesudasKM <you@example.com>
2024-04-06 17:37:36 +0530

    update

--- js/custom.js
+++ js/custom.js
@@ -71,17 +71,3 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 });
 
-
-// Product Images show
-
-document.addEventListener("DOMContentLoaded", function() {
-  const smallImages = document.querySelectorAll('.small-image');
-  const mainImage = document.getElementById('mainImage');
-
-  smallImages.forEach(function(smallImage) {
-    smallImage.addEventListener('mouseenter', function() {
-      const src = smallImage.getAttribute('src');
-      mainImage.setAttribute('src', src);
-    });
-  });
-});
