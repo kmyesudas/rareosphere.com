126da467d8cc18361d787143b02acb1f064b483b
YesudasKM <you@example.com>
2024-04-06 18:16:29 +0530

    update

--- js/custom.js
+++ js/custom.js
@@ -71,3 +71,15 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 });
 
+
+document.addEventListener("DOMContentLoaded", function() {
+  const smallImages = document.querySelectorAll('.small-image');
+  const mainImage = document.getElementById('mainImage');
+
+  smallImages.forEach(function(smallImage) {
+    smallImage.addEventListener('mouseenter', function() {
+      const src = smallImage.getAttribute('src');
+      mainImage.setAttribute('src', src);
+    });
+  });
+});
