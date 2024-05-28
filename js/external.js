6e48f3a4f074ad588315e41fb94aaaf3b09a3b62
YesudasKM <you@example.com>
2024-05-28 16:16:51 +0530

    update

--- js/external.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const links = document.querySelectorAll('a[href]');
-    links.forEach(link => {
-        const url = new URL(link.href, window.location.href);
-        if (url.hostname !== window.location.hostname) {
-            link.setAttribute('rel', 'noopener noreferrer');
-        }
-    });
-});
--- js/global.js
+++ js/global.js
@@ -32,3 +32,13 @@ document.addEventListener("DOMContentLoaded", function () {
     // Initial load
     showProducts();
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    const links = document.querySelectorAll('a[href]');
+    links.forEach(link => {
+        const url = new URL(link.href, window.location.href);
+        if (url.hostname !== window.location.hostname) {
+            link.setAttribute('rel', 'noopener noreferrer');
+        }
+    });
+});
